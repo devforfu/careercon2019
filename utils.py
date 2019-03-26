073983b713c078a665ce52a1fe3da5238d0ffea4
devforfu <developer.z@outlook.com>
2019-03-26 21:40:37 +0500

    Experiments with signal features

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import feather
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import StratifiedKFold
 
 from basedir import DATA
 
@@ -36,4 +37,14 @@ def dropcols(df, cols, missing_ok=True):
         cols = df.columns[df.columns.isin(cols)]
     return df.drop(columns=cols)
 
+
 def float64(data): return data.astype(np.float64)
+
+
+def split(data, target, n_splits=5, seed=None, verbose=True):
+    kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=seed)
+    idx = np.arange(len(data))
+    for i, (trn_idx, val_idx) in enumerate(kfold.split(idx, target), 1):
+        if verbose:
+            print(f'Running {i:d} of {kfold.get_n_splits():d} folds')
+        yield trn_idx, val_idx
