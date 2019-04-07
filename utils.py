cd108068954f6f9cc4330e579a5a26d6542afa88
devforfu <developer.z@outlook.com>
2019-04-07 19:36:49 +0500

    K-fold with k=10

--- utils.py
+++ utils.py
@@ -41,10 +41,22 @@ def dropcols(df, cols, missing_ok=True):
 def float64(data): return data.astype(np.float64)
 
 
-def split(data, target, n_splits=5, seed=None, verbose=True):
-    kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=seed)
+def split(data, target, n_splits=5, seed=None, verbose=True, fold_cls=None):
+    fold_cls = StratifiedKFold if fold_cls is None else fold_cls
+    kfold = fold_cls(n_splits=n_splits, shuffle=True, random_state=seed)
     idx = np.arange(len(data))
     for i, (trn_idx, val_idx) in enumerate(kfold.split(idx, target), 1):
         if verbose:
             print(f'Running {i:d} of {kfold.get_n_splits():d} folds')
         yield trn_idx, val_idx
+        
+        
+def kfolds(data, target, n_splits=5, seed=None, verbose=False):
+    splits = split(data, target, n_splits=n_splits, seed=seed, verbose=verbose)
+    for i, (trn_idx, val_idx) in enumerate(splits):
+        yield (
+            i,
+            data[data.index.isin(trn_idx)],
+            data[data.index.isin(val_idx)],
+            target[target.index.isin(trn_idx)],
+            target[target.index.isin(val_idx)])
